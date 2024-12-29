52f04c67a8ad0122ccdc053788b035b72bb17df6
erickwndl <erickwendel12@gmail.com>
2024-12-29 13:40:39 -0300

    first commit

---
+++ connection_handler.py
@@ -0,0 +1,119 @@
+from dataclasses import dataclass
+import pymysql.cursors
+from pymysql.cursors import DictCursor
+from pymysql import Connection
+
+
+@dataclass(kw_only=True)
+class connectionHandler:
+    host: str
+    username: str
+    password: str
+    database: str
+    cursor: DictCursor = None
+    conn: Connection = None
+
+    def __post_init__(self):
+        self.conn = self.criar_conexao()
+        self.cursor = self.conn.cursor()
+
+    def criar_conexao(self):
+        try:
+            conexao = pymysql.connect(
+                host=self.host,
+                port=46440,  # Adicione a porta explicitamente
+                user=self.username,
+                password=self.password,
+                database=self.database,
+                cursorclass=pymysql.cursors.DictCursor
+            )
+            print("Conexão estabelecida com sucesso!")
+            return conexao
+        except pymysql.MySQLError as e:
+            print(f"Erro ao conectar ao banco de dados: {e}")
+            raise
+
+    def insert(self, carro, placa, horario, dia, tipo_servico, peca_usada, valor_peca, valor_servico):
+        sql = "INSERT INTO servico (carro, placa, horario, dia, tipo_servico, peca_usada, valor_peca, valor_servico) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+        self.cursor.execute(sql, (carro, placa, horario, dia,
+                            tipo_servico, peca_usada, valor_peca, valor_servico))
+        self.conn.commit()
+
+    def listar_todos(self):
+        sql = "SELECT * FROM servico"
+        self.cursor.execute(sql)
+        servicos = self.cursor.fetchall()
+        return servicos
+
+    def buscar_por_id(self, id):
+        sql = "SELECT * FROM servico WHERE id = %s"
+        self.cursor.execute(sql, id)
+        servico = self.cursor.fetchone()
+        return servico
+    
+    def buscar_por_tipo(self, tipo_servico):
+        sql = "SELECT * FROM servico WHERE tipo_servico = %s"
+        self.cursor.execute(sql, tipo_servico)
+        servicos = self.cursor.fetchall()
+        return servicos
+
+
+    def buscar_por_carro(self, carro):
+        sql = "SELECT * FROM servico WHERE carro = %s"
+        self.cursor.execute(sql, carro)
+        servicos = self.cursor.fetchall()
+        return servicos
+    
+    def listar_por_data(self, data):
+        sql = "SELECT * FROM servico WHERE dia = %s"
+        self.cursor.execute(sql, (data,))
+        servicos = self.cursor.fetchall()
+        return servicos
+
+    def update(self, carro, placa, horario, dia, tipo_servico, peca_usada, valor_peca, valor_servico, id):
+        sql = "UPDATE servico SET carro = %s, placa =  %s, horario = %s, dia = %s, tipo_servico = %s, peca_usada = %s, valor_peca = %s, valor_servico = %s WHERE id = %s"
+        self.cursor.execute(sql, (carro, placa, horario, dia,
+                            tipo_servico, peca_usada, valor_peca, valor_servico, id))
+        self.conn.commit()
+
+    def delete(self, id):
+        sql = "DELETE FROM servico WHERE id = %s"
+        self.cursor.execute(sql, id)
+        self.conn.commit()
+
+    def calcular_total_dia(self, dia):
+        sql = "SELECT SUM(valor_servico) AS total_dia FROM servico WHERE dia = %s"
+        self.cursor.execute(sql, (dia,))
+        total = self.cursor.fetchone()
+        return total['total_dia'] if total['total_dia'] else 0
+
+    def listar_pecas(self):
+        sql = "SELECT * FROM pecas"
+        self.cursor.execute(sql)
+        pecas = self.cursor.fetchall()
+        return pecas
+
+    def adicionar_peca(self, nome, tipo, quantidade, preco):
+        sql = "INSERT INTO pecas (nome, tipo, quantidade, preco) VALUES (%s, %s, %s, %s)"
+        self.cursor.execute(sql, (nome, tipo, quantidade, preco))
+        self.conn.commit()
+
+    def atualizar_peca(self, id, nome, tipo, quantidade, preco):
+        sql = "UPDATE pecas SET nome = %s, tipo = %s, quantidade = %s, preco = %s WHERE id = %s"
+        self.cursor.execute(sql, (nome, tipo, quantidade, preco, id))
+        self.conn.commit()
+
+    # Remove uma peça
+    def remover_peca(self, id):
+        sql = "DELETE FROM pecas WHERE id = %s"
+        self.cursor.execute(sql, (id,))
+        self.conn.commit()
+
+
+connection_handler = connectionHandler(
+    host='autorack.proxy.rlwy.net',
+    username='root',
+    password='rCMuDIDgqqbEKubpBcdcfyDjasMGBVxN',
+    database='railway'
+)
+
---
+++ main.py
@@ -0,0 +1,158 @@
+from flask import (
+    Flask,
+    render_template,
+    redirect,
+    request,
+    url_for
+
+)
+
+from datetime import date
+
+
+from connection_handler import connection_handler
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def servicos_por_dia():
+    data = request.args.get('data', date.today().strftime('%Y-%m-%d'))
+    servico = connection_handler.listar_por_data(data)
+    total_dia = connection_handler.calcular_total_dia(data)
+    return render_template('servicos_dia.html', servico=servico, data=data, total_dia=total_dia)
+
+
+
+@app.route('/servico')
+def list_servicos():
+    servico = connection_handler.listar_todos()
+    return render_template('servicos.html', servico=servico)
+
+
+@app.route('/add-servico', methods=['GET', 'POST'])
+def add_servico():
+    if request.method == 'POST':
+        carro = request.form.get('carro')
+        placa = request.form.get('placa')
+        horario = request.form.get('horario')
+        dia = request.form.get('dia')
+        tipo = request.form.get('tipo_servico')
+        peca = request.form.get('peca_usada')
+        valor_peca = request.form.get('valor_peca')
+        valor_servico = request.form.get('valor_servico')
+        connection_handler.insert(
+            carro, placa, horario, dia, tipo, peca, valor_peca, valor_servico)
+        return redirect(url_for('list_servicos'))
+    return render_template('add-servico.html')
+
+
+@app.route('/deletar-servico/<id>')
+def deletar_servico(id: int):
+    connection_handler.delete(id)
+    return redirect(url_for('list_servicos'))
+
+
+@app.route('/atualizar-servico/<id>', methods=['GET', 'POST'])
+def update_servico(id: int):
+    servico = connection_handler.buscar_por_id(id)
+    if request.method == 'POST':
+        carro = request.form.get('carro')
+        placa = request.form.get('placa')
+        horario = request.form.get('horario')
+        dia = request.form.get('dia')
+        tipo = request.form.get('tipo_servico')
+        peca = request.form.get('peca_usada')
+        valor_peca = request.form.get('valor_peca')
+        valor_servico = request.form.get('valor_servico')
+        connection_handler.update(
+            carro, placa, horario, dia, tipo, peca, valor_peca, valor_servico, id)
+        return redirect(url_for('list_servicos'))
+    return render_template('update-servico.html', servico=servico)
+
+@app.route('/buscar', methods=['GET'])
+def buscar_servico():
+    tipo_servico = request.args.get('tipo_servico')
+    servicos = []
+    if tipo_servico:
+        # Use a função de busca no connection handler
+        servicos = connection_handler.buscar_por_tipo(tipo_servico)
+    return render_template('resultados_busca.html', servicos=servicos, tipo_servico=tipo_servico)
+
+
+
+
+@app.route('/buscar/<id>')
+def buscar_servico_id(id: int):
+    servico = connection_handler.buscar_por_id(id)
+    if request.method == 'POST':
+        carro = request.form.get('carro')
+        placa = request.form.get('placa')
+        horario = request.form.get('horario')
+        dia = request.form.get('dia')
+        tipo = request.form.get('tipo_servico')
+        peca = request.form.get('peca_usada')
+        valor_peca = request.form.get('valor_peca')
+        valor_servico = request.form.get('valor_servico')
+        connection_handler.update(
+            carro, placa, horario, dia, tipo, peca, valor_peca, valor_servico, id)
+        return redirect(url_for('list_servicos'))
+    return render_template('servico.html', servico=servico)
+
+@app.route('/estoque')
+def estoque_pecas():
+    pecas = connection_handler.listar_pecas()
+    return render_template('estoque.html', pecas=pecas)
+
+@app.route('/adicionar-peca', methods=['GET', 'POST'])
+def adicionar_peca():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        tipo = request.form['tipo']
+        quantidade = int(request.form['quantidade'])
+        preco = float(request.form['preco'])
+        connection_handler.adicionar_peca(nome, tipo, quantidade, preco)
+        return redirect(url_for('estoque_pecas'))
+    return render_template('adicionar_peca.html')
+
+@app.route('/editar-peca/<int:id>', methods=['GET', 'POST'])
+def editar_peca(id):
+    if request.method == 'POST':
+        nome = request.form['nome']
+        tipo = request.form['tipo']
+        quantidade = int(request.form['quantidade'])
+        preco = float(request.form['preco'])
+        connection_handler.atualizar_peca(id, nome, tipo, quantidade, preco)
+        return redirect(url_for('estoque_pecas'))
+    pecas = connection_handler.listar_pecas()
+    peca = next((p for p in pecas if p['id'] == id), None)
+    return render_template('editar_peca.html', peca=peca)
+
+@app.route('/pecas-em-falta')
+def pecas_em_falta():
+    pecas_faltando = [peca for peca in connection_handler.listar_pecas() if peca['quantidade'] == 0]
+    return render_template('pecas_em_falta.html', pecas=pecas_faltando)
+
+@app.route('/buscar-peca', methods=['GET', 'POST'])
+def buscar_peca():
+    peca = None
+    if request.method == 'POST':
+        id_peca = int(request.form['id'])
+        pecas = connection_handler.listar_pecas()
+        peca = next((p for p in pecas if p['id'] == id_peca), None)
+    return render_template('buscar_peca.html', peca=peca)
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
+    # connection_handler.close_connection()
+    # connection_handler.create_table()
+    # connection_handler.insert_sample_data()
+    # print(connection_handler.listar_todos())
+    # connection_handler.atualizar(1, 'Maria', 25, 'Python', 9.5)
+    # print(connection_handler.buscar_por_id(1))
+    # connection_handler.excluir(1)
